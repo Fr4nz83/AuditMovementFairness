61718a67b376662650d480dace1d89cc315959ac
Fr4nz83bis <francesco.lettich@humco.it>
2026-02-27 22:02:36 +0100

    Bringing the Bernoulli-based spatial scan statistic logic under an ad-hoc class.

--- src/bernoulli_spatial_scan.py
+++ src/bernoulli_spatial_scan.py
@@ -60,6 +60,28 @@ class BernoulliSpatialScan:
         return inside_positive_rate, outside_positive_rate, logL1, maxLogLR
 
 
+    def _reject_H0(self, vec_max_LR : np.ndarray, max_LR_dataset : float) :
+        '''
+        This function determines if the null H_0 must be rejected or not, according to the test statistic's empirical
+        distribution in 'vec_max_LR' and the test statistic's value in 'max_LR_dataset' computed from the real labels. 
+        '''
+        
+        # Determine where the max LR computed with the original labels fall in the empirical test statistic's distribution.
+        rank = np.count_nonzero(vec_max_LR >= max_LR_dataset)
+
+        # Monte Carlo p-value of the observed test statistic's value derived from the ranked empirical test statistic's distribution 
+        # (right tail), with +1 correction to include max_LR_dataset itself.
+        p_value = (rank + 1) / (self.num_simulations + 1)
+
+        # Based on the distribution and the real data we have, decide if we have to reject H_0.
+        reject_H0 = p_value <= self.alpha
+
+        # DEBUG: a few prints...
+        print(f"Position in sorted MC sample: {rank}/{self.num_simulations} (extreme if below position {int(self.num_simulations * self.alpha)})")
+        print(f"Monte Carlo p-value: {p_value:.6f}")
+        print(f"Decision: {'Reject H0' if reject_H0 else 'Do NOT reject H0'}")
+
+        return reject_H0
 
 
     ### PUBLIC CLASS CONSTRUCTOR ###
@@ -101,7 +123,7 @@ class BernoulliSpatialScan:
             raise ValueError("All candidates must have at least one object")
 
 
-    def sequential_simulations(self, labels : np.ndarray) :
+    def sequential_simulations(self, labels : np.ndarray) -> bool :
         ### SEQUENTIAL VERSION ###
 
         # 1 - Compute the L_0 likelihood, which models the likelihood of observing the labels in the data under the the assumption that the 
@@ -136,17 +158,44 @@ class BernoulliSpatialScan:
 
 
 
-        # 4 - Determine where the max LR computed with the original labels fall in the empirical test statistic's distribution.
-        rank = np.count_nonzero(vec_max_LR >= max_LR_dataset)
+        # 4 - Determine if we have to reject H_0 or not.
+        return self._reject_H0(vec_max_LR, max_LR_dataset)
 
-        # Monte Carlo p-value of the observed test statistic's value derived from the ranked empirical test statistic's distribution 
-        # (right tail), with +1 correction to include max_LR_dataset itself.
-        p_value = (rank + 1) / (self.num_simulations + 1)
 
-        # Based on the distribution and the real data we have, decide if we have to reject H_0.
-        reject_H0 = p_value <= self.alpha
+    def parallel_simulations(self, labels : np.ndarray) :
 
-        print(f"Statistical significance alpha: {self.alpha}")
-        print(f"Position in sorted MC sample: {rank}/{self.num_simulations} (extreme if below position {int(self.num_simulations * self.alpha)})")
-        print(f"Monte Carlo p-value: {p_value:.6f}")
-        print(f"Decision: {'Reject H0' if reject_H0 else 'Do NOT reject H0'}")
+        ### JOBLIB HELPER FUNCTION ###
+        def one_simulation(i, labels, flat_ids, indptr, lenghts, P, logL0_max):
+            rng = np.random.default_rng(i)
+            shuffled_labels = rng.permutation(labels)
+            return self._batch_max_likelihood_ratio(shuffled_labels, flat_ids, indptr, lenghts, P, logL0_max)[3]
+
+
+        ### JOBLIB MAIN CODE ###
+        # 1 - Compute the L_0 likelihood, which models the likelihood of observing the labels in the data under the the assumption that the 
+        # null hypotesis H_0 is true, i.e., there is a single global distribution that governs the labels. L_0 is constant across 
+        # permutations, since it depends only on the total number of positive and negative labels in the dataset, which does not change
+        # when shuffling the original labels.
+        P = labels.sum(dtype=np.uint32) # Overall number of objects with positive labels.
+        N = labels.size                 # Overall number of objects.
+        rho = P / N
+        logL0_max = P * np.log(rho) + (N - P) * np.log1p(-rho)
+
+        # 2 - Compute the simulations' max log-LRs in parallel.
+        vec_max_LR = Parallel(n_jobs=-1,
+                              backend="loky",
+                              verbose=10,
+                              #max_nbytes="1M",   # threshold that triggers auto-memmapping
+                              mmap_mode="r")(delayed(one_simulation)(i, labels, self.flat_ids, self.indptr, self.lenghts, P, logL0_max) for i in range(self.num_simulations))
+        vec_max_LR = np.asarray(vec_max_LR, dtype=np.float32)
+                
+
+        # 3 - Compute the max log likelihood ratio from the candidates when considering the original labels.
+        _, _, _, max_LR_dataset = self._batch_max_likelihood_ratio(labels,
+                                                                self.flat_ids, self.indptr, self.lenghts,
+                                                                P, logL0_max)
+
+
+
+        # 4 - Determine if we have to reject H_0 or not.
+        return self._reject_H0(vec_max_LR, max_LR_dataset)
